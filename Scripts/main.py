d7580cc694772c19fa43e9978dd824b9f4c0a1b7
wictorlopes <wictorlopes258@gmail.com>
2023-10-26 08:58:01 -0300

    alterações

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,19 +1,85 @@
 import openai
 from config import *
+from flask import Flask, request, jsonify
+import re
+
 
 openai.api_key = api_key
 
-# Função para gerar texto relacionado ao ODRL
-def generate_odrl_text(prompt):
+app = Flask(__name__)
+
+def read_file_odrl(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            odrl_info = file.read()
+        return odrl_info
+    except Exception as e:
+        print(f"Error reading ODRL file: {e}")
+        return None
+
+def build_prompt(odrl_info):
+    prompt = f"Extract the BUs from the ODRL document below:\n{odrl_info}\n\nExtracted BUs:"
+    return prompt
+
+
+def extract_bus_from_bnf_file(file_path):
+    # Padrão para identificar BUs
+    bu_pattern = r"BU_\d+\.bnf" 
+    bus = [] 
+
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+
+            # Use a expressão regular para encontrar correspondências
+            matches = re.findall(bu_pattern, content)
+
+            # Processar as correspondências
+            for match in matches:
+                bus.append(match)
+
+        return bus
+
+    except Exception as e:
+        print(f"Erro na leitura do arquivo .bnf: {e}")
+        return None
+
+def process_odrl_file(file_path):
+    odrl_info = read_file_odrl(file_path)
+    
+    prompt = build_prompt(odrl_info)
+    
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=50
+        max_tokens=150  
     )
-    return response.choices[0].text
+    
+    generated_text = response.choices[0].text
+    
+    extracted_bus = extract_bus_from_bnf_file(generated_text)
+    
+    return extracted_bus
 
-# Exemplo de uso
+@app.route('/generate-odrl-text', methods=['POST'])
+def generate_odrl_text():
+    data = request.get_json()
+    prompt = data.get('prompt')
+
+    if not prompt:
+        return jsonify({"error": "The 'prompt' parameter is missing from the request body"}), 400
+
+    return jsonify({"odrl_text": process_odrl_file(prompt)})
+
+
 if __name__ == "__main__":
-    prompt = "Gere uma descrição do ODRL:"
-    odrl_text = generate_odrl_text(prompt)
-    print(odrl_text)
+    arquivo_odrl = "../Examples/expl1/BU_0.bnf"
+    extracted_bu = process_odrl_file(arquivo_odrl)
+    
+    if extracted_bu:
+        print("Extracted BUs:")
+        for bu in extracted_bu:
+            print(bu)
+    else:
+        print("No BU was extracted.")
+
